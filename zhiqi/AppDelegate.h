dcc27dd484b14a2f68b22f4899b2d36295d9abf2
zhengmin <zhengmin@laiyouxi.com>
2024-04-03 17:54:25 +0800

    Project init

---
+++ zhiqi/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  zhiqi
+//
+//  Created by zm on 2024/4/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ zhiqi/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  zhiqi
+//
+//  Created by zm on 2024/4/3.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
